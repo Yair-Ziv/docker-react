da7e7c841440e1a38dda197a44f56bd83639f0dd
Yair Ziv <zivyair06@gmail.com>
2025-04-07 00:41:18 +0300

    Fixed text

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Lean Lulu/i);
+  const linkElement = screen.getByText(/Learn Docker/i);
   expect(linkElement).toBeInTheDocument();
 });
